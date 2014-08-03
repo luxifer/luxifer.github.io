b9783400c009e63ffdc65e7aaee47a46b3733951
Florent Viel <florent.viel69@gmail.com>
2014-08-03 11:47:09 +0200

    Nouveau design :boom:

--- config.rb
+++
@@ -1,9 +0,0 @@
-require 'bootstrap-sass'
-require 'pygments'
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "_sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
-output_style = :compressed
-line_comments = false
